c25c3de8127f154507851779eac49e2d05f38636
rabahbedirina <bedirina.rabah1993@gmail.com>
2020-11-30 21:43:49 +0100

    fix the /bin/bash

--- addcccamtoOscam.sh
+++ addcccamtoOscam.sh
@@ -1,4 +1,4 @@
-#! bin/bash
+#!/bin/bash
 # script to add cccam cline to oscam.server ( to replace old cline with new host port username and password)
 file_contents=/mnt/Studio/docker/.config/oscam/oscam/oscam.server
 
@@ -9,4 +9,4 @@ while read label host port user password; do
     sed -i "/$label/,/password/c label                         = $label\nenable                        = 1\nprotocol                      = cccam\ndevice                        = $host,$port\nuser                          = $user\npassword                      = $password " $file_contents
 
 done <$clines
-docker restart oscam
+#docker restart oscam
--- getCccam.sh
+++ getCccam.sh
@@ -1,11 +1,11 @@
-#! bin/bash
+#!/bin/bash
 # requirement package curl grep 
 
 curl https://www.cccambird.com/freecccam.php |echo 'cccambird' $( grep -Po '(?<=C: )[^<]*') > /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
 curl https://www.cccamprime.com/cccam48h.php |echo 'cccamprime' $( grep -Po '(?<=C: )[^<]*')  >> /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
 curl http://cccamgoo.com/free5/get2.php |echo 'cccamgoo' $( grep -Po '(?<=C: )[^<]*') >> /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
 curl https://cccamiptv.co/index.php/free-cccam/ |echo 'cccamiptv' $( grep -Po '(?<=C: )[^<]*') >> /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
-curl https://iptvfree.us/cccamfree/get.php | echo 'iptvfree'$(grep -o "cccam.*free.us") >> /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
+curl https://iptvfree.us/cccamfree/get.php | echo 'iptvfree' $(grep -o "cccam.*free.us") >> /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
 cat /mnt/Studio/docker/.config/oscam/scripts/cccam.cfg
 
 #curl https://cccamx.com/free-cccam.php |echo 'cccamx' $( grep -Po '(?<=Host: </strong> )[^<strong>]*\|(?<=Port: </strong> )[^<strong>]*')
