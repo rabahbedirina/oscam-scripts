59a33e8cf6d6594712edb41228c69d238eb88fd4
rabahbedirina <bedirina.rabah1993@gmail.com>
2021-09-17 20:33:29 +0100

    add readers

--- getCccam.sh
+++ getCccam.sh
@@ -4,13 +4,15 @@
 curl https://www.cccambird.com/freecccam.php |echo 'cccambird' $( grep -Po '(?<=C: )[^<]*') > ~/docker/.config/oscam/scripts/cccam.cfg
 curl https://www.cccamprime.com/cccam48h.php |echo 'cccamprime' $( grep -Po '(?<=C: )[^<]*')  >> ~/docker/.config/oscam/scripts/cccam.cfg
 #curl http://cccamgoo.com/free5/get2.php |echo 'cccamgoo' $( grep -Po '(?<=C: )[^<]*') >> ~/docker/.config/oscam/scripts/cccam.cfg
-#curl https://cccamiptv.co/index.php/free-cccam/ |echo 'cccamiptv' $( grep -Po '(?<=C: )[^<]*') >> ~/docker/.config/oscam/scripts/cccam.cfg
-#curl https://iptvfree.us/cccamfree/get.php | echo 'iptvfree' $(grep -o "cccam.*free.us") >> ~/docker/.config/oscam/scripts/cccam.cfg
-#curl http://cccamprima.com/free5/get2.php | echo 'cccamprima' $(grep -o 'free.cccamprima.*free' )>> ~/docker/.config/oscam/scripts/cccam.cfg
+curl https://cccamiptv.cc/free-cccam/ |echo 'cccamiptv' $( grep -Po '(?<=C: )[^<]*') >> ~/docker/.config/oscam/scripts/cccam.cfg
+curl http://cccambank.com/free5/get2.php | echo 'cccambank' $( grep -Po '(?<=C: )[^<]*') >> ~/docker/.config/oscam/scripts/cccam.cfg
+#curl http://cccambank.com/free5/get2.php | echo 'cccambank' $(grep -o "cccam.*free.us") >> ~/docker/.config/oscam/scripts/cccam.cfg
+curl http://cccamprima.com/free5/get2.php | echo 'cccamprima' $( grep -Po '(?<=C: )[^<]*')>> ~/docker/.config/oscam/scripts/cccam.cfg
+curl https://cccam.net/free | echo 'cccamnet' $( grep -Po '(?<=C: )[^<]*')>> ~/docker/.config/oscam/scripts/cccam.cfg
+#curl https://cccamx.com/v2/free-cccam | echo 'cccamx' $( grep -Po '(?<=C: )[^<]*')>> ~/docker/.config/oscam/scripts/cccam.cfg
 
-
+#http://free.cccamtiger.com/freecccam48h.php
 
 cat ~/docker/.config/oscam/scripts/cccam.cfg
 
-#curl https://cccamx.com/free-cccam.php |echo 'cccamx' $( grep -Po '(?<=Host: </strong> )[^<strong>]*\|(?<=Port: </strong> )[^<strong>]*')
 
